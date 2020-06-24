69e5580def602dd1a3544d766d159aadf0fc66dc
manuel <manuel@sahores.com>
2020-06-24 20:28:11 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{width: 200, borderBottomColor: 'black', borderBottomWidth: 2, marginBottom: 10}} />
+        <Button title='ADD' />
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   backgroundColor: 'red',
+   height: '100'
+ } 
 });
+
+export default App;
