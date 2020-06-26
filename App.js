80e66543cd5150742cafd39fb16564a2872f2201
toshiba <manuel@sahores.com>
2020-06-25 22:51:22 -0300

    ScrollView

--- App.js
+++ App.js
@@ -1,65 +1,69 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-
-//2:07
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	ScrollView
+} from "react-native";
 
 const App = () => {
-  const [enteredGoal, setEnteredGoal] = useState('');
-  const [goals, setGoals] = useState([]);
+	const [enteredGoal, setEnteredGoal] = useState("");
+	const [goals, setGoals] = useState([]);
 
-  const goalInputHandler = enteredText => {
-    setEnteredGoal(enteredText)
-  }
+	const goalInputHandler = enteredText => {
+		setEnteredGoal(enteredText);
+	};
 
-  const addGoalHandler = () => {
-    setGoals([...goals, enteredGoal]);
-    setEnteredGoal('');
-  }
+	const addGoalHandler = () => {
+		setGoals([...goals, enteredGoal]);
+		//setEnteredGoal("");
+	};
 
-  return (
-    <View style={styles.root}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-				<Button
-				  title='ADD'
-	        onPress={addGoalHandler}
-		  	  style={styles.btn}
+	return (
+		<View style={styles.root}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder="Course Goal"
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
 				/>
-      </View>
-      <View>
-        {goals.map((goal, index) => (
-								<View key={index}>
-												<Text>
-																{goal}
-												</Text>
-								</View>
+				<Button title="ADD" onPress={addGoalHandler} style={styles.btn} />
+			</View>
+			<ScrollView>
+				{goals.map((goal, index) => (
+					<View key={index} style={styles.listItem}>
+						<Text>{goal}</Text>
+					</View>
 				))}
-      </View>
-    </View>
-  );
-}
+			</ScrollView>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  root: {
-    padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'
-  },
-  input: {
-    width: 200,
-    borderColor: 'grey',
-    borderWidth: 1,
-    padding: 10
-  },
-  btn: {
-	  padding: 100,  //TODO: no toma este atributo
-  }
+	root: {
+		padding: 50
+	},
+	inputContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "flex-end"
+	},
+	input: {
+		width: 200,
+		borderColor: "grey",
+		borderWidth: 1,
+		padding: 10
+	},
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		borderWidth: 1,
+		backgroundColor: "#CCC"
+	}
 });
 
 export default App;
