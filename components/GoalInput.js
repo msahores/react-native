fd1dbe075d727cb5b4ebc326eb866fb73efff577
toshiba <manuel@sahores.com>
2020-06-29 15:52:32 -0300

    Goal Input updated

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,37 +1,43 @@
 import React, { useState } from 'react';
 import { View, TextInput, StyleSheet, Button } from 'react-native';
 
-const GoalInput = ({addGoalHandler}) => {
-  const [enteredText, setEnteredText] = useState();
+const GoalInput = ({ addGoalHandler }) => {
+	const [enteredText, setEnteredText] = useState();
 
-  const changeTextHandler = text => {
-    setEnteredText(text)
-  }
+	const changeTextHandler = text => {
+		setEnteredText(text);
+	};
 
-  return(
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Course Goal"
-        style={styles.input}
-        onChangeText={changeTextHandler}
-        defaultValue={enteredText}
-      />
-      <Button title="ADD" onPress={addGoalHandler.bind(this, enteredText)} style={styles.btn} />
-    </View>
-  )}
+	const onAdd = () => {
+		setEnteredText('');
+		addGoalHandler(enteredText);
+	};
+
+	return (
+		<View style={styles.inputContainer}>
+			<TextInput
+				placeholder='Course Goal'
+				style={styles.input}
+				onChangeText={changeTextHandler}
+				value={enteredText}
+			/>
+			<Button title='ADD' onPress={onAdd} style={styles.btn} />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	inputContainer: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "flex-end"
-  },
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'flex-end'
+	},
 	input: {
 		width: 200,
-		borderColor: "grey",
+		borderColor: 'grey',
 		borderWidth: 1,
 		padding: 10
-	},
-})
+	}
+});
 
 export default GoalInput;
